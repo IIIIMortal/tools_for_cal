379a99b7d95bac88daa8836446a78d136ec23fa5
haoge1234 <kevinyuhao-he@hotmail.com>
2021-03-22 00:59:09 +0800

    improve Gaussian_eli_first unit is zero

--- 计算方法/Gaussian_eli.m
+++ 计算方法/Gaussian_eli.m
@@ -4,13 +4,21 @@ function x = Gaussian_eli(A,rows)
     x = zeros(1,rows);
     m = zeros(1,rows);
     while(i<=rows)
-        %%从这里开始是
+        %%从这里开始是开始迭代
+	%这个地方是用于寻找列主元
         max_col_i = max(A(:,i-1));
         max_l = find(abs(A(:,i-1) -max_col_i)<0.01); 
         if max_l ~= i-1
+	    %交换两列
             A([max_l i-1],:) = A([i-1 max_l],:);
         end
-        %%
+	%%如果变换之后仍然是首项为0，则该循环直接跳过，并且方程可能直接无解
+        if abs(A(i-1, i-1) - 0)<0.01
+		continue;
+	end
+	%%
+
+	%找到主元之后，对每一行求系数以将最左列消去
         for j = i:rows
             m(j) = A(j,i-1)/A(i-1,i-1);
             A(j,:) = A(j,:) - m(j)*A(i-1,:);
