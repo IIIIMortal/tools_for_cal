4b7a7d1678e633ae06ad407f4cc1c5cc0282a161
haoge1234 <kevinyuhao-he@hotmail.com>
2021-03-23 17:05:13 +0800

    update explanation.md

--- 计算方法/Lab_1.m
+++ 计算方法/Lab_1.m
@@ -1,12 +1,18 @@
 clc;
 clear;
 close all;
-
+% 
 r = input("输入方阵行数 : ");
 a = input("输入矩阵 :\n");
 
-% result = Gaussian_eli(a,r);
+% r = 4;
+% a = [1 2 3 -4 -2; -3 -4 -12 13 5; 2 10 0 -3 10; 4 14 9 -13 7];
+%%
 [L,u,x] = Tri_decomposition(a,r);
 disp(L);
 disp(x);
 disp(u);
+
+
+% result = Gaussian_eli(a,r);
+% disp(result);
--- 计算方法/Tri_decomposition.m
+++ 计算方法/Tri_decomposition.m
@@ -5,32 +5,33 @@ function [L,u,x] = Tri_decomposition(A,rows)
     s = zeros(rows);%这里定义一个s用于放置列主元判断时的量
     
     for k=1:rows
-       %%
+       %%这个部分是（列主元）用的，如果不需要列主元，只是单纯的三角分解，就可以跳过这一部分（8~24行）
         s(k:rows,k) = u(k:rows,k);%赋值，在s中判断
         for i = k:rows
             s(i,k)=A(i,k)-u(i,1:k-1) * u(1:k-1,k);
         end
         
         max_u = max(s(k:rows,k));%找到最大值
-        
-        max_u_r = find(abs(s(:,k)-max_u)<0.01,1); 
-        if max_u ~= k
+%         max_u
+        max_u_r = find(abs(s(:,k)-max_u)<0.01,1); % 找到最大值index
+        if max_u_r ~= k%如果和当前不同就交换
             u([max_u_r k],:)=u([k max_u_r],:);
             A([max_u_r k],:)=A([k max_u_r],:);
         end
+
 %         clear s_i;
 %         k
 %         disp(u);
         %%
         for j=k:rows + 1
-            u(k,j)=A(k,j)-u(k,1:k-1) * u(1:k-1,j);
+            u(k,j)=A(k,j)-u(k,1:k-1) * u(1:k-1,j);%这里用了矩阵的乘法，乘出来是一个常数
         end
         for i=k+1:rows
             u(i,k)=(A(i,k)-u(i,1:k-1) * u(1:k-1,k))/u(k,k);
         end
         
     end
-    
+    %后面的是把x，L，U求出来
     y = u(:,rows+1);
     u = u(:,1:rows);
     x = zeros(rows,1);
